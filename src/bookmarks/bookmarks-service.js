773e8b800821b98ab9f0251539cc1518295b4c96
klguenth <klguenth@gmail.com>
2019-10-22 20:33:24 -0500

    updated test code and endpoints

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -1,30 +1,29 @@
-
 const BookmarksService = {
     getAllBookmarks(knex) {
-        return knex.select('*').from('bookmarks')
-    },
-    insertBookmark(knex, newBookmark) {
-        return knex
-            .insert(newBookmark)
-            .into('bookmarks')
-            .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
+      return knex.select('*').from('bookmarks')
     },
     getById(knex, id) {
-        return knex.from('bookmarks').select('*').where('id', id).first()
+      return knex.from('bookmarks').select('*').where('id', id).first()
+    },
+    insertBookmark(knex, newBookmark) {
+      return knex
+        .insert(newBookmark)
+        .into('bookmarks')
+        .returning('*')
+        .then(rows => {
+          return rows[0]
+        })
     },
     deleteBookmark(knex, id) {
-        return knex('bookmarks')
-            .where({ id })
-            .delete()
+      return knex('bookmarks')
+        .where({ id })
+        .delete()
     },
     updateBookmark(knex, id, newBookmarkFields) {
-        return knex('bookmark')
-            .where({ id })
-            .update(newBookmarkFields)
+      return knex('bookmarks')
+        .where({ id })
+        .update(newBookmarkFields)
     },
-}
-
-module.exports = BookmarksService
+  }
+  
+  module.exports = BookmarksService
